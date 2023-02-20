d93bec90b7d4275e5247814fecca462f56063789
Tony G. Bolaño <tony@tonygb.com>
2023-02-20 22:13:47 +0100

    feat: tenemos esqueleto de la aplicación

---
+++ balance/views.py
@@ -0,0 +1,33 @@
+from . import app
+
+
+@app.route('/')
+def home():
+    """
+    Muestra la lista de movimientos cargados
+    """
+    return "Lista de movimientos"
+
+
+@app.route('/nuevo')
+def add_movement():
+    """
+    Crea un movimiento nuevo y lo guarda en el archivo CSV
+    """
+    return "Crea movimiento"
+
+
+@app.route('/modificar')
+def update():
+    """
+    Permite editar los datos de un movimiento.
+    """
+    return "Actualizar movimiento"
+
+
+@app.route('/borrar')
+def delete():
+    """
+    Elimina un movimiento existente
+    """
+    return "Eliminar movimiento"
